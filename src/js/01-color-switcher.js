5da4e53f7acf5ef838db24caaca4de926a510540
Illia Kazmin <kazmin1c@gmail.com>
2022-09-06 17:24:19 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+let changeBackgroundBodyIntervalId = null;
 
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+refs.startBtn.classList.add('btn');
+refs.stopBtn.classList.add('btn');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  changeBackgroundBodyIntervalId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(changeBackgroundBodyIntervalId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
