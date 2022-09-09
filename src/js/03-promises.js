b4996fc0a387d5746397d542c05bc2fbd42e856e
Illia Kazmin <kazmin1c@gmail.com>
2022-09-09 08:27:34 +0200

    added task 3 of HW9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    console.log('In create promise ', position, delay, shouldResolve);
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(evt) {
+  console.log('Enter a submit');
+  evt.preventDefault();
+
+  const {
+    elements: [delayEl, stepEl, amountEl],
+  } = evt.currentTarget;
+
+  let delay = Number(delayEl.value);
+  const step = Number(stepEl.value);
+  const amount = Number(amountEl.value);
+
+  console.log(`Delay: ${delay}, step: ${step}, amount: ${amount} `);
+
+  for (let i = 1; i <= amount; i += 1) {
+    // console.log(i, delay);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
