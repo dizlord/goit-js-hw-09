13d5401512940041a75399c21a781123df80e25f
Illia Kazmin <kazmin1c@gmail.com>
2022-09-09 08:27:07 +0200

    converted timer into the class

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,37 +12,71 @@ const refs = {
   secondsEl: document.querySelector('span[data-seconds]'),
 };
 
+class backCounter {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.onTick = onTick;
+  }
+
+  start(finishTime) {
+    this.intervalId = setInterval(() => {
+      const timeToFinish = this.convertMs(finishTime - Date.now());
+      const { days, hours, minutes, seconds } = timeToFinish;
+      if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+        clearInterval(this.intervalId);
+      }
+      this.onTick(
+        this.addLeadingZero(days),
+        this.addLeadingZero(hours),
+        this.addLeadingZero(minutes),
+        this.addLeadingZero(seconds)
+      );
+    }, 1000);
+  }
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  addLeadingZero(value = '') {
+    return String(value).padStart(2, '0');
+  }
+}
+
 let finishTime = {};
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+function initIntrerface(
+  days = '00',
+  hours = '00',
+  minutes = '00',
+  seconds = '00'
+) {
+  refs.daysEl.textContent = days;
+  refs.hoursEl.textContent = hours;
+  refs.minutesEl.textContent = minutes;
+  refs.secondsEl.textContent = seconds;
+  // if (timer.isActive) {
+  refs.startBtn.disabled = true;
+  refs.dateInputEl.disabled = true;
+  // }
 }
 
-function addLeadingZero(value = '') {
-  return String(value).padStart(2, '0');
-}
-
-function initIntrerface({ days = 0, hours = 0, minutes = 0, seconds = 0 }) {
-  refs.daysEl.textContent = addLeadingZero(days);
-  refs.hoursEl.textContent = addLeadingZero(hours);
-  refs.minutesEl.textContent = addLeadingZero(minutes);
-  refs.secondsEl.textContent = addLeadingZero(seconds);
-}
+const timer = new backCounter({ onTick: initIntrerface });
 
 const options = {
   enableTime: true,
@@ -51,7 +85,6 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const currentDate = Date.now();
-
     if (currentDate < selectedDates[0]) {
       refs.startBtn.disabled = false;
       finishTime = selectedDates[0];
@@ -62,23 +95,11 @@ const options = {
   },
 };
 
+flatpickr(refs.dateInputEl, options);
+
 refs.startBtn.disabled = true;
 refs.startBtn.classList.add('btn-counter');
 
-flatpickr(refs.dateInputEl, options);
-
-refs.startBtn.addEventListener('click', onStartClick);
-
-function onStartClick() {
-  const intervalId = setInterval(() => {
-    const timeToFinish = convertMs(finishTime - Date.now());
-    const { days, hours, minutes, seconds } = timeToFinish;
-    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-      clearInterval(intervalId);
-    }
-    initIntrerface(timeToFinish);
-  }, 1000);
-
-  refs.startBtn.disabled = true;
-  refs.dateInputEl.disabled = true;
-}
+refs.startBtn.addEventListener('click', () => {
+  timer.start(finishTime);
+});
